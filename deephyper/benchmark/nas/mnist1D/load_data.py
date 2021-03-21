74f55fdbcd384ee34a72ee60b30fe81fc84550b9
Hongyuan Liu <lliuhy@gmail.com>
2021-03-21 02:08:10 +0000

    update

--- deephyper/benchmark/nas/mnist1D/load_data.py
+++ deephyper/benchmark/nas/mnist1D/load_data.py
@@ -1,6 +1,9 @@
 import os
-from tensorflow.examples.tutorials.mnist import input_data
+# from tensorflow.examples.tutorials.mnist import input_data
 import numpy as np
+import tensorflow as tf
+import keras
+from keras.datasets import mnist
 
 HERE = os.path.dirname(os.path.abspath(__file__))
 
@@ -10,29 +13,29 @@ def load_data(prop=0.1):
     """Loads the MNIST dataset.
     Returns Tuple of Numpy arrays: `(train_X, train_y), (valid_X, valid_y)`.
     """
-    dest = HERE+'/DATA'
 
-    mnist = input_data.read_data_sets(dest, one_hot=True)
+    dest = os.path.join('/home/hyliu/work/datasets/mnistmlp', 'mnist.npz')
+    print('load data from : ', dest)
 
-    train_X = mnist.train.images / 255
-    train_y = mnist.train.labels
+    (x_train, y_train), (x_test, y_test) = mnist.load_data(dest)
 
-    valid_X = mnist.validation.images / 255
-    valid_y = mnist.validation.labels
+    x_train = x_train.reshape(60000, 784)
+    x_test = x_test.reshape(10000, 784)
+    x_train = x_train.astype('float32')
+    x_test = x_test.astype('float32')
+    x_train /= 255
+    x_test /= 255
 
-    # Subset selection for training
-    train_size = np.shape(train_X)[0]
-    limit = int(train_size*prop)
-    indexes = np.array([i for i in range(train_size)])
-    np.random.shuffle(indexes)
-    train_X = train_X[indexes[:limit]]
-    train_y = train_y[indexes[:limit]]
+    y_train = keras.utils.to_categorical(y_train, 10)
+    y_test = keras.utils.to_categorical(y_test, 10)
 
-    print(f'train_X shape: {np.shape(train_X)}')
-    print(f'train_y shape: {np.shape(train_y)}')
-    print(f'valid_X shape: {np.shape(valid_X)}')
-    print(f'valid_y shape: {np.shape(valid_y)}')
-    return (train_X, train_y), (valid_X, valid_y)
+    print(f'train_X shape: {np.shape(x_train)}')
+    print(f'train_y shape: {np.shape(y_train)}')
+    print(f'valid_X shape: {np.shape(x_test)}')
+    print(f'valid_y shape: {np.shape(y_test)}')
+
+    return (x_train, y_train), (x_test, y_test)
+
 
 if __name__ == '__main__':
     load_data()
--- deephyper/benchmark/nas/mnist1D/problem.py
+++ deephyper/benchmark/nas/mnist1D/problem.py
@@ -1,10 +1,66 @@
 from deephyper.problem import NaProblem
 from deephyper.benchmark.nas.mnist1D.load_data import load_data
 from deepspace.tabular import OneLayerFactory
+from deephyper.nas.space.auto_keras_search_space import AutoKSearchSpace
+from deephyper.nas.space.node import ConstantNode, VariableNode
+from deephyper.nas.space.op.op1d import (Activation, Conv1D,
+                                                      Dense, Dropout, Flatten,
+                                                      Identity, MaxPooling1D)
+
+
+def add_conv_op_(node):
+    # node.add_op(Identity())
+    node.add_op(Conv1D(filter_size=3, num_filters=8))
+    node.add_op(Conv1D(filter_size=4, num_filters=8))
+    node.add_op(Conv1D(filter_size=5, num_filters=8))
+    node.add_op(Conv1D(filter_size=6, num_filters=8))
+
+def add_dense_op_(node):
+    # node.add_op(Identity())
+    node.add_op(Dense(units=10))
+    node.add_op(Dense(units=50))
+    node.add_op(Dense(units=100))
+    node.add_op(Dense(units=200))
+    node.add_op(Dense(units=250))
+    node.add_op(Dense(units=500))
+    node.add_op(Dense(units=750))
+    node.add_op(Dense(units=1000))
+
+def add_activation_op_(node):
+    # node.add_op(Identity())
+    node.add_op(Activation(activation='relu'))
+    node.add_op(Activation(activation='tanh'))
+    node.add_op(Activation(activation='sigmoid'))
+
 
 
 def create_search_space(input_shape=(10,), output_shape=(1,), **kwargs):
-    return OneLayerFactory()(input_shape, output_shape, **kwargs)
+    struct = AutoKSearchSpace(input_shape, output_shape, regression=False)
+
+    # n1 = VariableNode('N1ConvOp')
+    # add_conv_op_(n1)
+    # struct.connect(struct.input_nodes[0], n1)
+
+    n00 = VariableNode('N8DenseOp')
+    add_dense_op_(n00)
+    struct.connect(struct.input_nodes[0], n00)
+
+    n90 = VariableNode('N9ActivOp')
+    add_activation_op_(n90)
+    struct.connect(n00, n90)
+
+
+    n8 = VariableNode('N8DenseOp')
+    add_dense_op_(n8)
+    struct.connect(n90, n8)
+
+    n9 = VariableNode('N9ActivOp')
+    add_activation_op_(n9)
+    struct.connect(n8, n9)
+
+    return struct
+
+    # return OneLayerFactory()(input_shape, output_shape, **kwargs)
 
 
 Problem = NaProblem()
@@ -14,7 +70,7 @@ Problem.load_data(load_data)
 Problem.search_space(create_search_space)
 
 Problem.hyperparameters(
-    batch_size=100, learning_rate=0.1, optimizer="adam", num_epochs=10
+    batch_size=32, learning_rate=0.01, optimizer="adam", num_epochs=10
 )
 
 Problem.loss("categorical_crossentropy")
