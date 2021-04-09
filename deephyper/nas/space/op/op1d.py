99c8489b1b6f6c91741fc71236cee2196bed1418
Hongyuan Liu <lliuhy@gmail.com>
2021-04-09 20:03:56 +0000

    activation in conv1d

--- deephyper/nas/space/op/op1d.py
+++ deephyper/nas/space/op/op1d.py
@@ -84,12 +84,13 @@ class Conv1D(Operation):
         padding (str): 'same' or 'valid'
     """
 
-    def __init__(self, filter_size, num_filters=1, strides=1, padding='same'):
+    def __init__(self, filter_size, num_filters=1, strides=1, padding='same', **kwargs):
         self.filter_size = filter_size
         self.num_filters = num_filters
         self.strides = strides
         self.padding = padding
         self._layer = None
+        self._kwargs = kwargs
 
     def __str__(self):
         return f'{type(self).__name__}_{self.filter_size}_{self.num_filters}'
@@ -107,7 +108,9 @@ class Conv1D(Operation):
                 filters=self.num_filters,
                 kernel_size=self.filter_size,
                 strides=self.strides,
-                padding=self.padding)
+                padding=self.padding,
+                **self._kwargs)
+                
         out = self._layer(out)
         return out
 
